eb9d6e10fa22c008e258238107d07a9dcfc2b155
Alexandr Chivarzin <admin@sccraft.ru>
2022-03-02 09:28:53 +0300

    Разделение чата на входящие и отправленные (только дизайн)
    Сейчас все сообщения по прежнему выводятся в отправленных.
    Входящие всегда пустые.

--- Приложение/WahChat/Forms/Chat.Designer.cs
+++ Приложение/WahChat/Forms/Chat.Designer.cs
@@ -33,13 +33,14 @@
             this.SendButton = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Button();
             this.chatBox = new System.Windows.Forms.ListBox();
+            this.chatInbox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
             this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.usernameLabel.Location = new System.Drawing.Point(28, 24);
+            this.usernameLabel.Location = new System.Drawing.Point(11, 9);
             this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(51, 22);
@@ -48,18 +49,21 @@
             // 
             // messageBox
             // 
+            this.messageBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.messageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.messageBox.Location = new System.Drawing.Point(32, 282);
-            this.messageBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.messageBox.Location = new System.Drawing.Point(3, 296);
+            this.messageBox.Margin = new System.Windows.Forms.Padding(2);
             this.messageBox.Name = "messageBox";
-            this.messageBox.Size = new System.Drawing.Size(258, 28);
+            this.messageBox.Size = new System.Drawing.Size(313, 28);
             this.messageBox.TabIndex = 11;
             // 
             // SendButton
             // 
+            this.SendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.SendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SendButton.Location = new System.Drawing.Point(298, 282);
-            this.SendButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SendButton.Location = new System.Drawing.Point(320, 299);
+            this.SendButton.Margin = new System.Windows.Forms.Padding(2);
             this.SendButton.Name = "SendButton";
             this.SendButton.Size = new System.Drawing.Size(140, 25);
             this.SendButton.TabIndex = 12;
@@ -70,8 +74,8 @@
             // closeButton
             // 
             this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.closeButton.Location = new System.Drawing.Point(298, 24);
-            this.closeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.closeButton.Location = new System.Drawing.Point(320, 11);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(140, 25);
             this.closeButton.TabIndex = 13;
@@ -81,26 +85,43 @@
             // 
             // chatBox
             // 
+            this.chatBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.chatBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.chatBox.FormattingEnabled = true;
             this.chatBox.ItemHeight = 17;
-            this.chatBox.Location = new System.Drawing.Point(32, 74);
-            this.chatBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chatBox.Location = new System.Drawing.Point(199, 72);
+            this.chatBox.Margin = new System.Windows.Forms.Padding(2);
             this.chatBox.Name = "chatBox";
-            this.chatBox.Size = new System.Drawing.Size(408, 191);
+            this.chatBox.Size = new System.Drawing.Size(261, 191);
             this.chatBox.TabIndex = 14;
+            // 
+            // chatInbox
+            // 
+            this.chatInbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.chatInbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chatInbox.FormattingEnabled = true;
+            this.chatInbox.ItemHeight = 17;
+            this.chatInbox.Location = new System.Drawing.Point(3, 72);
+            this.chatInbox.Margin = new System.Windows.Forms.Padding(2);
+            this.chatInbox.Name = "chatInbox";
+            this.chatInbox.Size = new System.Drawing.Size(184, 191);
+            this.chatInbox.TabIndex = 15;
             // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(471, 335);
+            this.Controls.Add(this.chatInbox);
             this.Controls.Add(this.chatBox);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.SendButton);
             this.Controls.Add(this.messageBox);
             this.Controls.Add(this.usernameLabel);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Chat";
             this.Text = "Чат";
             this.ResumeLayout(false);
@@ -115,5 +136,6 @@
         private System.Windows.Forms.Button SendButton;
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.ListBox chatBox;
+        private System.Windows.Forms.ListBox chatInbox;
     }
 }
