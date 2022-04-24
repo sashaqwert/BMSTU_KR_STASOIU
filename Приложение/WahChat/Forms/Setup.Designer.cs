1920bf66fd5df2fd7005a9015b1b2508c6807a8c
Alexandr Chivarzin <admin@sccraft.ru>
2022-04-24 13:10:32 +0300

    Заготовка для выбора скорости передачи
    В настоящее время списки пустые.
    Их значения не проверяются.

--- Приложение/WahChat/Forms/Setup.Designer.cs
+++ Приложение/WahChat/Forms/Setup.Designer.cs
@@ -35,6 +35,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.connectButton = new System.Windows.Forms.Button();
             this.checkBox = new System.Windows.Forms.CheckBox();
+            this.box_speed1 = new System.Windows.Forms.ComboBox();
+            this.box_speed2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // incomePortBox
@@ -52,14 +56,12 @@
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
-            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
             this.label1.Name = "label1";
             // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
             this.label2.Name = "label2";
             // 
             // connectButton
@@ -75,18 +77,43 @@
             this.checkBox.Name = "checkBox";
             this.checkBox.UseVisualStyleBackColor = true;
             // 
+            // box_speed1
+            // 
+            this.box_speed1.FormattingEnabled = true;
+            resources.ApplyResources(this.box_speed1, "box_speed1");
+            this.box_speed1.Name = "box_speed1";
+            // 
+            // box_speed2
+            // 
+            this.box_speed2.FormattingEnabled = true;
+            resources.ApplyResources(this.box_speed2, "box_speed2");
+            this.box_speed2.Name = "box_speed2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
             // Setup
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.box_speed2);
+            this.Controls.Add(this.box_speed1);
             this.Controls.Add(this.checkBox);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.outcomePortBox);
             this.Controls.Add(this.incomePortBox);
-            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Setup";
             this.Load += new System.EventHandler(this.Setup_Load);
@@ -103,6 +130,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button connectButton;
         private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.ComboBox box_speed1;
+        private System.Windows.Forms.ComboBox box_speed2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
