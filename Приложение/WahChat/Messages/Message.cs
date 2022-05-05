2467cb17c07349ec3ec8ab94ceb1efe1c6ee87f4
Alexandr Chivarzin <admin@sccraft.ru>
2022-05-05 09:40:46 +0300

    Добавлен класс Message

---
+++ Приложение/WahChat/Messages/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WahChat.Messages
+{
+    public class Message
+    {
+        public string msgTime;
+        public int authorID;
+        public string msgText;
+
+        public Message(string time, int author, string msg)
+        {
+            this.msgTime = time;
+            this.authorID = author;
+            this.msgText = msg;
+        }
+    }
+}
